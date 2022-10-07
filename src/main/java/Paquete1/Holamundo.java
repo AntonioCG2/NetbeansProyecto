78b415365973bcf8485a4d440231674fdc5a7b68
AntonioCG2 <antoniocg382@gmail.com>
2022-10-07 18:08:06 +0200

    HolaMundo

---
+++ src/main/java/Paquete1/Holamundo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Paquete1;
+
+/**
+ *
+ * @author 1daw
+ */
+public class Holamundo {
+        public static void main(String[] args) {
+System.out.println("¡Hola mundo!");
+    }
+}
